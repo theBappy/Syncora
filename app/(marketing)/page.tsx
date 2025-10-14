08fbae72e437d7f9e8f719f3cd1cd41486bc847d
theBappy <abappy575@gmail.com>
2025-10-14 13:38:20 +0600

    landing page ui

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,10 @@
+import HeroSection from "@/components/hero/hero-section";
+
+
 export default function Home() {
   return (
     <div className="">
-      
+      <HeroSection />
     </div>
   )
 }
